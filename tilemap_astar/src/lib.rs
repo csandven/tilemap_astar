1a9e3ce6d2f90dfcd7ef6c084c8c971acdf2b7a4
Christian Sandven <christian.sandven@amedia.no>
2023-05-07 14:42:54 +0200

    Remove unused trait

--- tilemap_astar/src/lib.rs
+++ tilemap_astar/src/lib.rs
@@ -4,10 +4,6 @@ pub mod grid;
 pub mod node;
 pub mod pos;
 
-pub trait PathSolver {
-  fn solve(&self, start: Pos, target: Pos) -> Option<Vec<Pos>>;
-}
-
 pub mod prelude {
   pub use crate::grid::*;
   pub use crate::node::*;
